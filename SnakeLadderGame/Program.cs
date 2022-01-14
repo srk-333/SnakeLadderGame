b3a7f2eff3410da5adf14eeeac13726056bbe325
srk-333 <sauravmk333@gmail.com>
2022-01-14 18:06:08 +0530

    [Saurav]Added Welcome message

---
+++ SnakeLadderGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Game Program");
+            Console.ReadLine();
+        }
+    }
+}
