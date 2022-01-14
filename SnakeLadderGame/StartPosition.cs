b4be2a69aec3dcd902a0337a38e84d94ff5706a9
srk-333 <sauravmk333@gmail.com>
2022-01-14 19:09:08 +0530

    [Saurav]Added Game Movement program

--- SnakeLadderGame/StartPosition.cs
+++ SnakeLadderGame/StartPosition.cs
@@ -10,13 +10,28 @@ namespace SnakeLadderGame
     {
         //Instance Variables.
         int position = 0;
+        const int MAX_POSITION = 100;
+        const int LADDER = 1;
+        const int SNAKE = 2;
         Random random = new Random();       // object of random class
         //Method
         public void RunGame()
         {
-            Console.WriteLine("Player position is: " + position);
-            int randomNum = random.Next(1,7);         //random number berween 1 to 6
-            Console.WriteLine("randomNum: "+randomNum);
+            Console.WriteLine("Player position is: " + position);      
+            int diceValue = random.Next(1, 7);          //random number berween 1 to 6 for Dice Value.
+            int playCheck = random.Next(0,3);          //Generating random value between 0 to 2 to control movement of player position
+            switch (playCheck)
+            { 
+                 case LADDER:
+                     Console.WriteLine("Move player position ahed Dice Value ");
+                     break;
+                 case SNAKE:
+                     Console.WriteLine("Move player position behind Dice Value ");
+                     break;
+                 default:
+                     Console.WriteLine("player stays at his position ");
+                     break;
+            }
         }
     }
 }
