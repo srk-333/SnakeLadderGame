fcb0a77d179e33148e99ac42570f05c8e927578c
srk-333 <sauravmk333@gmail.com>
2022-01-14 19:51:19 +0530

    [Saurav]Added Loop to reach Max position

--- SnakeLadderGame/StartPosition.cs
+++ SnakeLadderGame/StartPosition.cs
@@ -11,27 +11,41 @@ namespace SnakeLadderGame
         //Instance Variables.
         int position = 0;
         const int MAX_POSITION = 100;
+        const int MIN_POSITION = -1;
         const int LADDER = 1;
         const int SNAKE = 2;
         Random random = new Random();       // object of random class
         //Method
         public void RunGame()
         {
-            Console.WriteLine("Player position is: " + position);      
-            int diceValue = random.Next(1, 7);          //random number berween 1 to 6 for Dice Value.
-            int playCheck = random.Next(0,3);          //Generating random value between 0 to 2 to control movement of player position
-            switch (playCheck)
-            { 
-                 case LADDER:
-                     Console.WriteLine("Move player position ahed Dice Value ");
-                     break;
-                 case SNAKE:
-                     Console.WriteLine("Move player position behind Dice Value ");
-                     break;
-                 default:
-                     Console.WriteLine("player stays at his position ");
-                     break;
+            Console.WriteLine("Player position is: " + position);
+            //Using While Loop to iterate Game till position reaches max position.
+            int i = 0;
+            while( position <= MAX_POSITION )
+            {
+                int diceValue = random.Next(1, 7);          //random number berween 1 to 6 for Dice Value.
+                int playCheck = random.Next(0, 3);          //Generating random value between 0 to 2 to control movement of player position
+                if (position <= MIN_POSITION)
+                {
+                    position = 0;
+                }
+                else
+                {
+                    switch (playCheck)
+                    {
+                        case LADDER:
+                            position += diceValue;
+                            break;
+                        case SNAKE:
+                            position -= diceValue;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                i++;
             }
+            Console.WriteLine("Position after end of game: "+position);
         }
     }
 }
