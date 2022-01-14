45fdcc288fff32c3925b69331ce854beea054304
srk-333 <sauravmk333@gmail.com>
2022-01-14 18:34:22 +0530

    [Saurav]Added random number grnerating progran

--- SnakeLadderGame/StartPosition.cs
+++ SnakeLadderGame/StartPosition.cs
@@ -10,10 +10,13 @@ namespace SnakeLadderGame
     {
         //Instance Variables.
         int position = 0;
+        Random random = new Random();       // object of random class
         //Method
         public void RunGame()
         {
             Console.WriteLine("Player position is: " + position);
+            int randomNum = random.Next(1,7);         //random number berween 1 to 6
+            Console.WriteLine("randomNum: "+randomNum);
         }
     }
 }
